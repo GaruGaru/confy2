774bf7626229bf2501d092ede2ff08824a39bcb5
Your Name <garuglieritommaso@gmail.com>
2017-11-14 16:20:03 +0100

    Added cache persistance tests

--- src/test/java/TestProxyCache.java
+++ src/test/java/TestProxyCache.java
@@ -2,6 +2,7 @@ import it.modularity.confy.proxy.ProxyCache;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -13,11 +14,21 @@ public class TestProxyCache {
     }
 
     @Test
-    public void testCache() {
+    public void testCacheGet() {
         ProxyCache proxyCache = ProxyCache.create(Method::getName);
         Method method = String.class.getMethods()[0];
         Object value = proxyCache.get(method);
         assertThat(value).isEqualTo(method.getName());
     }
+
+    @Test
+    public void testCachePersist() {
+        ProxyCache proxyCache = ProxyCache.create(method -> UUID.randomUUID().toString());
+        Method method = String.class.getMethods()[0];
+        Object firstsValue = proxyCache.get(method);
+        for (int i = 0; i < 3; i++)
+            assertThat(proxyCache.get(method)).isEqualTo(firstsValue);
+    }
+
 
 }
